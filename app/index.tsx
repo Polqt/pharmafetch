6d849e0060165b12f4d264f4f80107ad9e10362c
Jepoy <poyhidalgo@gmail.com>
2025-06-12 04:13:48 +0800

    chore: update dependencies and add Tailwind CSS configuration
    - Added nativewind package for Tailwind CSS support.
    - Updated react-native-safe-area-context to use caret versioning.
    - Added tailwind.config.js for Tailwind CSS configuration.
    - Included nativewind-env.d.ts in tsconfig.json for TypeScript support.
    - Added prettier-plugin-tailwindcss and tailwindcss to devDependencies.

---
+++ app/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+import "./global.css"
+
+const index = () => {
+  return (
+    <View>
+      <Text className="text-red-500">Hello World</Text>
+    </View>
+  )
+}
+
+export default index
---
+++ components/ScreenWrapper.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const ScreenWrapper = () => {
+  return (
+    <View>
+      <Text>ScreenWrapper</Text>
+    </View>
+  )
+}
+
+export default ScreenWrapper
